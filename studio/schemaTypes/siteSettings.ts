a4f1887dc7c456c52e2cce717198ae6161735f2c
Kevin White <kevin@spiritmedia.us>
2026-03-08 18:31:26 -0400

    Wire all siteSettings socials (LinkedIn, Twitter) through Sanity in Footer

--- studio/schemaTypes/siteSettings.ts
+++ studio/schemaTypes/siteSettings.ts
@@ -1,16 +1,18 @@
 export default {
-  name: 'siteSettings',
-  title: 'Site Settings',
-  type: 'document',
-  __experimental_actions: ['update', 'publish'],
-  fields: [
-    { name: 'siteName', title: 'Site Name', type: 'string' },
-    { name: 'tagline', title: 'Tagline', type: 'string' },
-    { name: 'description', title: 'Site Description', type: 'text' },
-    { name: 'email', title: 'Contact Email', type: 'string' },
-    { name: 'phone', title: 'Phone', type: 'string' },
-    { name: 'facebook', title: 'Facebook URL', type: 'url' },
-    { name: 'instagram', title: 'Instagram URL', type: 'url' },
-    { name: 'youtube', title: 'YouTube URL', type: 'url' },
-  ],
-}
+	name: 'siteSettings',
+	title: 'Site Settings',
+	type: 'document',
+	__experimental_actions: ['update', 'publish'],
+	fields: [
+		{ name: 'siteName', title: 'Site Name', type: 'string' },
+		{ name: 'tagline', title: 'Tagline', type: 'string' },
+		{ name: 'description', title: 'Site Description', type: 'text' },
+		{ name: 'email', title: 'Contact Email', type: 'string' },
+		{ name: 'phone', title: 'Phone', type: 'string' },
+		{ name: 'facebook', title: 'Facebook URL', type: 'url' },
+		{ name: 'instagram', title: 'Instagram URL', type: 'url' },
+		{ name: 'youtube', title: 'YouTube URL', type: 'url' },
+		{ name: 'linkedin', title: 'LinkedIn URL', type: 'url' },
+		{ name: 'twitter', title: 'X / Twitter URL', type: 'url' },
+	],
+};
